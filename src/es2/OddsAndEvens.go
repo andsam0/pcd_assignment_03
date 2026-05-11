0819f9994e24029179eecf50011589c87654af56
Mattia Ronchi <mattia.ronchi5@studio.unibo.it>
2026-05-11 17:48:37 +0200

    finished ex2 with barrier

--- src/es2/OddsAndEvens.go
+++ src/es2/OddsAndEvens.go
@@ -5,51 +5,55 @@ import (
 	"math/rand/v2"
 	"os"
 	"strconv"
+	// "time"
 )
 
 type msg struct {
 	number int
-	reply  chan bool 
+	reply  chan result
 	id     int
 }
 
-func player(id int, ch chan msg) bool {
-	number := rand.IntN(10)
-	reply := make(chan bool)
-	ch <- msg{number, reply, id}
-	won := <-reply
-	if won {
-		fmt.Printf("  [player %2d] drew %d → advances\n", id, number)
-	} else {
-		fmt.Printf("  [player %2d] drew %d → eliminated\n", id, number)
-	}
-	return won
+type result struct {
+	won      bool
+	opponent int // player id
 }
 
-func tournament(id int, channels []chan msg, done chan struct{}) {
-	defer close(done)
-	for _, ch := range channels {
-		if !player(id, ch) {
+func player(id int, ch chan msg) result {
+	number := rand.IntN(10)
+	reply := make(chan result)
+	ch <- msg{number, reply, id}
+	result := <-reply
+	return result
+}
+
+func tournament(id int, channels []chan msg, barriers []chan struct{}) {
+	for i, ch := range channels {
+		result := player(id, ch)
+		if !result.won {
 			return
 		}
+		<-barriers[i]
+		fmt.Printf("Player %d wins against player %d in round %d\n", id, result.opponent, i)
 	}
 	fmt.Printf("\n Player %d wins the tournament!\n", id)
 }
 
-func judge(round, games int, ch chan msg) {
-	fmt.Printf("\n--- Round %d: %d concurrent game(s) ---\n", round, games)
+func judge(round, games int, ch chan msg, barrier chan struct{}) {
+	// fmt.Printf("\n--- Round %d: %d concurrent game(s) ---\n", round, games)
 	for i := 0; i < games; i++ {
 		m1 := <-ch
 		m2 := <-ch
 		sum := m1.number + m2.number
 		if sum%2 == 0 {
-			m1.reply <- true
-			m2.reply <- false
+			m1.reply <- result{true, m2.id}
+			m2.reply <- result{false, m1.id}
 		} else {
-			m1.reply <- false
-			m2.reply <- true
+			m1.reply <- result{false, m2.id}
+			m2.reply <- result{true, m1.id}
 		}
 	}
+	close(barrier)
 }
 
 func main() {
@@ -70,18 +74,19 @@ func main() {
 	for i := range channels {
 		channels[i] = make(chan msg)
 	}
+	barriers := make([]chan struct{}, m)
+	for i := range barriers {
+		barriers[i] = make(chan struct{})
+	}
 
-	dones := make([]chan struct{}, N)
 	for i := range N {
-		dones[i] = make(chan struct{})
-		go tournament(i+1, channels, dones[i])
+		go tournament(i+1, channels, barriers)
 	}
 
 	for r := range m {
-		judge(r+1, N>>(r+1), channels[r])
+		judge(r+1, N>>(r+1), channels[r], barriers[r])
 	}
 
-	for _, d := range dones {
-		<-d
-	}
+	// todo: trovare un modo per non terminare prima di stampare il vincitore del torneo
+	for{}
 }
